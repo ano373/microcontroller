598243509aa39d62f2f3c8fc8d5797b0bf1c8445
ano373 <96596021+ano373@users.noreply.github.com>
2023-06-18 02:19:10 +0300

    Update main.c
    now works with the 2 motors and in 4 bit mode combatinale with hamdy's kit only the temp sensor isnt and need to change its pin

--- main.c
+++ main.c
@@ -1,29 +1,22 @@
-/*
- * main.c
- *
- * Created: 4/15/2023 9:01:20 AM
- *  Author: ANO
- */ 
-
 #define F_CPU 16000000UL
 #define SET_BIT(REG,BIT) (REG |= (1<<BIT))
 #define RST_BIT(REG,BIT) (REG &= ~(1<<BIT))
 #define GET_BIT(REG,BIT) ((REG & (1<<BIT))>>BIT)
-#define send_upper(REG,BITS) (REG = (REG & 0x07) | ( (BITS>>1) & 0x78))
-#define send_lower(REG,BITS) (REG = (REG & 0x07) | ( (BITS<<3) & 0x78))
+#define send_upper(REG,BIT) ( REG=(REG & 0xE8)|(((BIT>>4) & 0x07) | (((BIT>>4) & 0x08)<<1) ) )
+#define send_lower(REG,BIT) ( REG=(REG & 0xE8)|((BIT & 0x07) | ((BIT & 0x08) << 1)) )
+
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 
-#define LCD PORTA
+#define LCD PORTB
 #define control_port PORTA
-#define RS 1
-#define E 2
+#define RS PA3
+#define E PA2
 #define RW 7
-#define green_pin 0
-#define yellow_pin 1
-#define red_pin 2
+#define Heater PD0
+#define AirConditioner PD1
 
 
 unsigned int data;
@@ -37,48 +30,52 @@ unsigned int adc_read(unsigned int channel);
 
 int main(void)
 {
-    LCD_init();
-    LCDprint("temp: "); 
+	LCD_init();
+	LCDprint("temp: ");
 	SET_BIT(DDRA,0);
-	SET_BIT(DDRD,green_pin);
-	SET_BIT(DDRD,yellow_pin);
-	SET_BIT(DDRD,red_pin);
+	SET_BIT(DDRB,PB0); // set lcd pins
+	SET_BIT(DDRB,PB1);
+	SET_BIT(DDRB,PB2);
+	SET_BIT(DDRB,PB4);
+	SET_BIT(DDRD,Heater);
+	SET_BIT(DDRD,AirConditioner);
+	
 	adc_init();
 	
 	
-    while (1)
+	while (1)
 	{
 		data = adc_read(0);
 		float temperature = (data * 5 / 1024.0) * 100.0;
 		char ASCi[16];
 		sprintf(ASCi, "%.2f", temperature);
 		LCDprint(ASCi);
+		
 		_delay_ms(500);
-		LCDcmd(0x01);
+		LCDcmd(0x06);	// move cursor to right
+		LCDcmd(0x01);	
 		
-		if (temperature <=25) 
+		if (temperature <=22 && temperature >=-15)
+		{
+			SET_BIT(PORTD, Heater);
+			RST_BIT(PORTD,AirConditioner);
+		}
+		else if (temperature >= 22 && temperature <28)
+		{
+			RST_BIT(PORTD, Heater);
+			RST_BIT(PORTD,AirConditioner);
+			
+		}
+		else if (temperature >= 29 && temperature <50)
+		{
+			RST_BIT(PORTD, Heater);
+			SET_BIT(PORTD,AirConditioner);
+		}
+		else if (temperature >= 51)
 			{
-			 SET_BIT(PORTD, green_pin);
-			 RST_BIT(PORTD,yellow_pin);
-			 RST_BIT(PORTD,red_pin);
-			}
-			 else if (temperature >= 26 && temperature <70) 
-			 {
-				 RST_BIT(PORTD, green_pin);
-				 SET_BIT(PORTD,yellow_pin);
-				 RST_BIT(PORTD,red_pin);
-			 }
-			 else if (temperature >= 70)
-			 {
-				RST_BIT(PORTD, green_pin);
-				RST_BIT(PORTD,yellow_pin);
-				SET_BIT(PORTD,red_pin);
-			 }
-		
-		
-		
-		
-		
+				RST_BIT(PORTD, Heater);
+				RST_BIT(PORTD,AirConditioner);
+			}						
 	}
 }
 
@@ -86,7 +83,7 @@ void pulse()
 {
 	SET_BIT(control_port,E);
 	_delay_ms(1);
-	 RST_BIT(control_port,E);
+	RST_BIT(control_port,E);
 }
 
 void LCD_init()
@@ -105,12 +102,12 @@ void LCD_init()
 
 void LCDcmd(unsigned char cmd)
 {
-	 RST_BIT(control_port,RS);
-	 RST_BIT(control_port,RW);
-	 send_upper(LCD,cmd);
-	 pulse();
-	 send_lower(LCD,cmd);
-	 pulse();
+	RST_BIT(control_port,RS);
+	RST_BIT(control_port,RW);
+	send_upper(LCD,cmd);
+	pulse();
+	send_lower(LCD,cmd);
+	pulse();
 	_delay_ms(5);
 }
 
@@ -129,11 +126,11 @@ void LCDdata(unsigned char data)
 
 void LCDprint(const char *str)
 {
-    while (*str)
-    {
-        LCDdata(*str++);
+	while (*str)
+	{
+		LCDdata(*str++);
 		_delay_ms(10);
-    }
+	}
 }
 
 void adc_init(){
